9f23b0a9a68a41fb8b5617f33f3620e13f852d35
abhmora1011 <59785830+abhmora1011@users.noreply.github.com>
2022-05-14 12:30:52 +0800

    Create base structure of cucumber using selenium java and adding some required files

---
+++ cucumberJavaV1/src/test/java/testRunner/TestRunner.java
@@ -0,0 +1,20 @@
+package testRunner;
+
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)				
+@CucumberOptions(
+		features="src/test/java/Features",
+		glue= {"stepDefinitions"},
+		plugin = { "json:target/cucumber-report/cucumber.json",
+        "html:target/cucumber-report/cucumber.html"},
+		monochrome = true
+		)	
+
+public class TestRunner {
+
+}
