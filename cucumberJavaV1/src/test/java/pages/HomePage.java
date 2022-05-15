fe546ea3363589dd6b24b330ed836890eb9540a8
abhmora1011 <59785830+abhmora1011@users.noreply.github.com>
2022-05-15 13:42:47 +0800

    Refactor script for a cleaner and readable code and add plugin that is needed build a report

--- cucumberJavaV1/src/test/java/pages/HomePage.java
+++ cucumberJavaV1/src/test/java/pages/HomePage.java
@@ -3,7 +3,9 @@ package pages;
 import java.time.Duration;
 import java.util.List;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,34 +14,29 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import testdata.TestData;
 
-public class HomePage extends TestData{
-	
+public class HomePage extends TestData {
+
 	WebDriver driver;
-	
+
 	public HomePage(WebDriver driver) {
 		this.driver = driver;
 	}
-	
+
 	By txt_PersonCol = By.id("GridFrow2fltPerson-awed");
 	By drop_dateFilter = By.id("GridFrow2fltDate-awed");
 	By drop_ListOfYear = By.xpath("//*[@id='GridFrow2fltDate-dropmenu']/div[2]/ul/li");
 	By drop_foodFilter = By.id("GridFrow2fltFood-awed");
-	By drop_ListOfFood = By.xpath("//*[@id='GridFrow2fltFood-dropmenu']/div[2]/ul/li/div"); 
-	
+	By drop_ListOfFood = By.xpath("//*[@id='GridFrow2fltFood-dropmenu']/div[2]/ul/li");
+
 	public void enterName(String name) {
-//		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-//		WebElement personCol = wait
-//				.until(ExpectedConditions.visibilityOfElementLocated(this.txt_PersonCol));
-		
 		WebElement personCol = explicitWait(txt_PersonCol);
 		personCol.sendKeys(name);
 		personCol.sendKeys(Keys.ENTER);
 	}
-	
+
 	public void selectYear(String year) {
-//		WebElement dateFilter = wait
-//				.until(ExpectedConditions.visibilityOfElementLocated(By.id("GridFrow2fltDate-awed")));
-//		dateFilter.click();
+
+		setYear = year;
 		
 		WebElement dateFilter = explicitWait(drop_dateFilter);
 		dateFilter.click();
@@ -47,7 +44,6 @@ public class HomePage extends TestData{
 		List<WebElement> foodYear = driver.findElements(drop_ListOfYear);
 
 		for (WebElement yrOption : foodYear) {
-			System.out.println("----" + yrOption.getText());
 			if (yrOption.getText().equalsIgnoreCase(year)) {
 				selectedYear = yrOption.getText();
 				yrOption.click();
@@ -56,18 +52,17 @@ public class HomePage extends TestData{
 		}
 
 	}
-	
-	public void selectFood(String food) {
-//		WebElement foodCol = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("GridFrow2fltFood-awed")));
-//		foodCol.click();
 
+	public void selectFood(String food) {
+		
+		selectedFood = food;
+		
 		WebElement foodCol = explicitWait(drop_foodFilter);
 		foodCol.click();
-		
+
 		List<WebElement> foods = driver.findElements(drop_ListOfFood);
-		
+
 		for (WebElement option : foods) {
-			System.out.println("----" + option.getText());
 			if (option.getText().equalsIgnoreCase(food)) {
 				selectedFood = option.getText();
 				option.click();
@@ -75,10 +70,30 @@ public class HomePage extends TestData{
 			}
 		}
 	}
-	
+
 	public WebElement explicitWait(By element) {
-	    WebDriverWait w = new WebDriverWait(driver, Duration.ofSeconds(10));
-	    return w.until(ExpectedConditions.visibilityOfElementLocated(element));
+		WebDriverWait w = new WebDriverWait(driver, Duration.ofSeconds(10));
+		return w.until(ExpectedConditions.visibilityOfElementLocated(element));
+	}
+
+	public void pause(int milliseconds) throws InterruptedException {
+		Thread.sleep(milliseconds);
+	}
+
+	public void scrollPageByValue(int pixelValue1, int pixelValue2) {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scroll( " + pixelValue1 + "," + pixelValue2 + ")");
+	}
+
+	public void assertValue(String expected, String actual) {
+		Assert.assertEquals(expected, actual);
+	}
+
+	public void tearDown() {
+		driver.quit();
 	}
 	
+	public void launchBrowser(String url) {
+		driver.get(url);
+	}
 }
