e2f6b0f7b0773ae5a515b52b2102e7c8a5713f7b
abhmora1011 <59785830+abhmora1011@users.noreply.github.com>
2022-05-15 15:12:46 +0800

    Modidy POM.xml to have some plugin to generate report

--- cucumberJavaV1/src/test/java/testRunner/TestRunner.java
+++ cucumberJavaV1/src/test/java/testRunner/TestRunner.java
@@ -6,8 +6,8 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/Features", glue = { "stepDefinitions" }, plugin = {
-		"json:target/cucumber-report/cucumber.json", "html:target/cucumber-report/cucumber.html" }, monochrome = true)
+@CucumberOptions(features = "src/test/java/Features", glue = { "stepDefinitions" }, 
+plugin = {"json:target/cucumber.json"}, monochrome = true)
 
 public class TestRunner {
 
